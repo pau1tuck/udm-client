9c2bec3d2a748d6a54b96ac09a0e1315fe16d0f9
Paul Tuck <paul.tuck@oxon.org>
2023-05-27 16:16:59 +0700

    "2023-05-27_16:16+07"

---
+++ web/src/utils/global-state-context.tsx
@@ -0,0 +1,110 @@
+import React, { ReactNode, createContext, useContext, useReducer } from "react";
+
+export enum ViewMode {
+    GRID = "grid",
+    LIST = "list",
+}
+
+export interface GlobalState {
+    viewMode: ViewMode;
+    nowPlaying: boolean;
+    trackId: string;
+}
+
+export interface GlobalStateAction {
+    type: "TOGGLE_VIEW_MODE" | "SET_NOW_PLAYING" | "SET_TRACK_ID";
+    payload?: any;
+}
+
+const initialState: GlobalState = {
+    viewMode: ViewMode.GRID,
+    nowPlaying: false,
+    trackId: "",
+};
+
+const GlobalStateContext = createContext<GlobalState | undefined>(undefined);
+const GlobalDispatchContext = createContext<
+    React.Dispatch<GlobalStateAction> | undefined
+>(undefined);
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
+
+const globalStateReducer = (
+    state: GlobalState,
+    action: GlobalStateAction
+): GlobalState => {
+    switch (action.type) {
+        case "TOGGLE_VIEW_MODE":
+            return {
+                ...state,
+                viewMode:
+                    state.viewMode === ViewMode.GRID
+                        ? ViewMode.LIST
+                        : ViewMode.GRID,
+            };
+        case "SET_NOW_PLAYING":
+            return {
+                ...state,
+                nowPlaying: action.payload,
+            };
+        case "SET_TRACK_ID":
+            return {
+                ...state,
+                trackId: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+// * * * * * * * * * * * * * * * * * * * * * * * * *
+
+interface GlobalStateProviderProps {
+    children: ReactNode;
+}
+
+const GlobalStateProvider = ({ children }: GlobalStateProviderProps) => {
+    const [state, dispatch] = useReducer(globalStateReducer, initialState);
+
+    const toggleViewMode = () => {
+        dispatch({ type: "TOGGLE_VIEW_MODE" });
+    };
+
+    const setNowPlaying = (value: boolean) => {
+        dispatch({ type: "SET_NOW_PLAYING", payload: value });
+    };
+
+    const setTrackId = (id: string) => {
+        dispatch({ type: "SET_TRACK_ID", payload: id });
+    };
+
+    return (
+        <GlobalStateContext.Provider value={state}>
+            <GlobalDispatchContext.Provider value={dispatch}>
+                {children}
+            </GlobalDispatchContext.Provider>
+        </GlobalStateContext.Provider>
+    );
+};
+
+const useGlobalState = (): GlobalState => {
+    const context = useContext(GlobalStateContext);
+    if (context === undefined) {
+        throw new Error(
+            "useGlobalState must be used within a GlobalStateProvider"
+        );
+    }
+    return context;
+};
+
+const useGlobalDispatch = (): React.Dispatch<GlobalStateAction> => {
+    const context = useContext(GlobalDispatchContext);
+    if (context === undefined) {
+        throw new Error(
+            "useGlobalDispatch must be used within a GlobalStateProvider"
+        );
+    }
+    return context;
+};
+
+export { GlobalStateProvider, useGlobalDispatch, useGlobalState };
