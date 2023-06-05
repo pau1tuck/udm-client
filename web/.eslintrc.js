d9bced7ca28b800f43852f135622340f67bb1907
Paul Tuck <paul.tuck@oxon.org>
2023-06-05 10:42:10 +0700

    "2023-06-05_10:42+07"

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -20,10 +20,8 @@ module.exports = {
         "plugin:react-hooks/recommended", // Uses the recommended rules from @eslint-plugin-react-hooks
         "plugin:jsx-a11y/recommended",
         "next",
-        "next/babel",
         "next/core-web-vitals",
         "plugin:tailwindcss/recommended",
-        "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
     plugins: ["import", "sort-keys-fix", "simple-import-sort"],
